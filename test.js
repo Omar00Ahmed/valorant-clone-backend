3be8b0d3c3d63f411d597bd75780c7b4399353db
Omar Ahmed <livaiakramen4767@gmail.com>
2024-11-21 19:32:19 +0200

    updated

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
+const path = require('path');
 
 fs.mkdir('public', { recursive: true }, (err) => {
     if (err) throw err;
-    console.log("done");
+    fs.copyFile(path.join(__dirname, 'index.js'), path.join(__dirname, 'public', 'index.js'), (err) => {
+        if (err) throw err;
+        console.log("done");
+    });
 });  
